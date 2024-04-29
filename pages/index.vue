6b3460069e8e8d3059678be8de294a7039d0bc5a
kauzkoko <kamelkebab@tutanota.com>
2024-04-29 12:43:53 +0200

    feat: zeige em beni

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-beni">
+        <div @click="increment()" >hey beni</div>
+        <div :style="{transform: `scale${increment}`}">{{counter}}</div>
+    </div>
+</template>
+
+<script setup>
+const counter = ref(0)
+const increment = () => counter.value++
+</script>
+
+<style scoped>
+.container-beni {
+    background-color: red;
+}
+</style>
